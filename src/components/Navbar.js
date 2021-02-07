9b86108f3791aed4320371b67a473f400ea944bc
csdj92 <csdj92@gmail.com>
2021-02-07 12:09:25 -0500

    Navbar for home and campgrounds

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => { 
+    return (
+        <div>
+            <nav className="navbar navbar-light" 
+            style={{
+                background: '#000',
+                height: '100px'
+                                }}>
+                <Link to='/'>
+                    <h1>Home</h1>
+                </Link>
+                <Link to='/campgrounds'>
+                    <h1>Campgrounds</h1>
+                </Link>
+            </nav>
+        </div>
+    )
+}
+export default Navbar

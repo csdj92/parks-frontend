878fdda6f77264b5872cc0ce2e0ededf15da635a
csdj92 <csdj92@gmail.com>
2021-02-07 12:10:18 -0500

    API call for parks

---
+++ src/actions/parksActions.js
@@ -0,0 +1,8 @@
+export const fetchParks = () => {
+    return(dispatch) => {
+        dispatch({ type: 'LOADING_PARKS'})
+        fetch('http://localhost:3000/parks')
+            .then(response => { return response.json() })
+            .then(data => { dispatch({ type: "ADD_PARKS", parks: data })})
+    }
+}

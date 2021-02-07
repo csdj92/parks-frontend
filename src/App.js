f40aad4c723e7ccee7f03128d273383793b8bbf1
csdj92 <csdj92@gmail.com>
2021-02-07 12:12:49 -0500

    seperated concerns

--- src/App.js
+++ src/App.js
@@ -2,49 +2,59 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { fetchParks } from './actions/parksActions'
+import { fetchCampgrounds } from './actions/CampgroundsActions' 
 import { connect } from 'react-redux'
+import ParksRow from './components/ParksRow'
+import CampgroundsRow from './components/CampgroundRow'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ParkSearch from './components/ParkSearch'
+import Park from './components/Park'
+import NavBar from './components/Navbar'
+import Campground from './components/Campground';
 
 
+class App extends Component {
 
-
-export default class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     parks: []
-  //   }
-  // }
-
- 
-
-  componentDidMount() {
-    fetch('http://localhost:3000/parks')
-    .then(response => response.json())
-    .then(data => this.setState({ parks: data.slice(0,10) }))
-    }
-    //{this.state.parks.map((parks, index) => <img src={parks.images.split("}, {",)[2].split('"url"=>"')[1].split('"')[0]} alt='parks' key={index}></img>)}
-
-  noPic = () => {
-  let parks = this.state.parks
-  parks = parks.filter((park) => park.images===[]); console.log(parks)
-}
-
+componentDidMount() {
+    this.props.fetchParks();     
+    this.props.fetchCampgrounds() 
+  }
   render() {   
-    console.log(this.state) 
-    //debugger
-    return(   
-
-      
-        
-      <div className='container-fluid parks-app'>
-			<div className='row'>
-        {this.state.parks.map((park, index) => <div className = "item" key={index}>{park.fullName}<br></br>
-        <img src={park.images.split("}, {",)[0].split('"url"=>"')[1].split('"')[0]} alt='parks' key={index}></img><p>{park.description}</p></div>)}
-        
-      </div>
-        </div>
-        
+    return(
+      <Router>
+      <NavBar/>
+      <Route exact path='/' component={ParkSearch}/>
+      <Route exact path="/parks/:id" component={Park} />
+      <Route exact path="/" component={ParksRow}/> 
+      <Route exact path="/campgrounds/:id" component={Campground} />
+      <Route exact path="/campgrounds" component={CampgroundsRow}/>
+      </Router> 
     )
   }
 } 
-// get data from state then map through each park to find if any == null
+
+
+
+const mapStateToProps = state => {
+  return {
+    parks: state.parks,
+    campgrounds: state.campgrounds,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchParks: () => dispatch(fetchParks()),
+    fetchCampgrounds: () => dispatch(fetchCampgrounds())
+  }
+}
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+//{this.state.parks.map((parks, index) => <img src={parks.images.split("}, {",)[2].split('"url"=>"')[1].split('"')[0]} alt='parks' key={index}></img>)}
